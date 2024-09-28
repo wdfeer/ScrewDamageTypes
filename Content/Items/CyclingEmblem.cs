4e9537249589b9f7299717650de4bf25964f0f1f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-28 18:00:09 +0200

    Make CyclingEmblem also give +15% damage

--- Content/Items/CyclingEmblem.cs
+++ Content/Items/CyclingEmblem.cs
@@ -1,12 +1,16 @@
 using ScrewDamageTypes.Content.Player;
 using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace ScrewDamageTypes.Content.Items;
 
 public class CyclingEmblem : ModItem
 {
+	public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs((int)(DAMAGE_INCREASE * 100f));
+	private const float DAMAGE_INCREASE = 0.15f;
+	
 	public override void SetDefaults()
 	{
 		// TODO: create custom texture and update width, height
@@ -26,9 +30,10 @@ public class CyclingEmblem : ModItem
 		recipe.AddTile(TileID.MythrilAnvil);
 		recipe.Register();
 	}
-
+	
 	public override void UpdateAccessory(Terraria.Player player, bool hideVisual)
 	{
+		player.GetDamage(DamageClass.Generic).Flat += DAMAGE_INCREASE;
 		player.GetModPlayer<CyclingPlayer>().Enabled = true;
 	}
 }
